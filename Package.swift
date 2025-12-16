1561eb5e6c006007763d3cee17b52b43c7498562
danilpapa <rexportydf@mail.ru>
2025-12-16 14:59:26 +0300

    clean up

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
     name: "Blade",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Blade",
             targets: ["Blade"]
@@ -19,12 +18,9 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0-latest"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "BladeMacros",
             dependencies: [
@@ -32,12 +28,7 @@ let package = Package(
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "Blade", dependencies: ["BladeMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
         .executableTarget(name: "BladeClient", dependencies: ["Blade"]),
-
     ]
 )
